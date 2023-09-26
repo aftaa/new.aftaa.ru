a5706da38349277fd97aa1e628391b2ecabb5197
Maxim Gabidullin <after@ya.ru>
2023-09-26 08:34:07 +0300

    +top@index

--- public/js/aftaa.ru/index.js
+++ public/js/aftaa.ru/index.js
@@ -1,14 +1,44 @@
 $(function () {
-    window.functions = {
-        getData: async function () {
-            window.data = await jwtFetch('/private/index');
-            if (jwtSuccess()) {
-                console.log(data);
-            } else {
-                modal.show();
+        window.functions = {
+            getData: async function () {
+                window.data = await jwtFetch('/private/index');
+                if (jwtSuccess()) {
+                    let vm = new Vue({
+                        el: '#app',
+                        data: {
+                            columns: {},
+                            top: {},
+
+                            debug: true,
+                            requestDataFail: false,
+                            status: 200,
+
+                            seen: true,
+                        },
+                        methods: {
+                            conversion(event) {
+                                let id = event.target.dataset.id;
+                                (async function () {
+                                    await jwtFetch('/private/link/view/' + id);
+                                })();
+                            },
+                            loadIndexData() {
+                                vm.columns = data.data.columns;
+                                vm.top = data.data.top;
+                                console.log(data)
+                            }
+                        }
+                    });
+
+                    vm.loadIndexData();
+                } else {
+                    modal.show();
+                }
             }
         }
-    };
+        ;
 
-    (async () => await functions.getData())();
-});
+        (async () => await functions.getData())();
+    }
+)
+;
