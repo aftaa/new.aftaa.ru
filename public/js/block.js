7d6d2cf265d728568ec00f9e6a003da07b45dc99
Maxim Gabidullin <after@ya.ru>
2023-09-27 16:18:34 +0300

    +blank-code@block.new

--- public/js/block.js
+++ public/js/block.js
@@ -7,17 +7,30 @@ $(function () {
             col: 0,
             sort: 0,
             private: false,
+
+            modal: new bootstrap.Modal('#modalBlock')
         },
         methods: {
             new: function () {
 
             },
 
-            add: function() {
+            add: function () {
 
             },
 
-            load: function () {
+            load: async function (event) {
+                let id = event.target.dataset.id;
+                let t = this;
+                jwtFetch('/blocks/' + id)
+                    .then((data) => {
+                        t.id = data.id;
+                        t.name = data.name;
+                        t.col = data.col;
+                        t.sort = data.sort;
+                        t.private = data.private;
+                        this.modal.show();
+                    });
             },
 
             save: function () {
