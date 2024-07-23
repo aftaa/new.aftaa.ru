19eb7b003bf9862384617fc40e092c834c0fb1d3
Maksim Gabidullin <maksim.gabidullin@umbrellait.com>
2024-07-23 21:55:51 +0300

    klaro

--- public/js/klaro.config.js
+++ public/js/klaro.config.js
@@ -1,66 +1,68 @@
-// By default, Klaro will load the config from  a global "klaroConfig" variable.
-// You can change this by specifying the "data-config" attribute on your
-// script take, e.g. like this:
-// <script src="klaro.js" data-config="myConfigVariableName" />
 var klaroConfig = {
     acceptAll: true,
-    translations: {
-
-    },
     services: [
-        // {
-        //     name: 'test',
-        //     required: true,
-        //     purpose: ['site-cookies']
-        // },
         {
             name: 'google-tag-manager',
             required: true,
-            purposes: ['marketing', 'functional'],
-            manages: ['google-analytics'],
+            purposes: ['marketing'],
             onAccept: `
                 // we notify the tag manager about all services that were accepted. You can define
                 // a custom event in GTM to load the service if consent was given.
                 for(let k of Object.keys(opts.consents)){
                     if (opts.consents[k]){
                         let eventName = 'klaro-'+k+'-accepted'
-                        console.log(eventName)
                         dataLayer.push({'event': eventName})
                     }
-                }
-                // if consent for Google Analytics was granted we enable analytics storage
-                if (opts.consents[opts.vars.googleAnalyticsName || 'google-analytics']){
-                    console.log("Google analytics usage was granted")
-                    gtag('consent', 'update', {'analytics_storage': 'granted'})
-                }
-                // if consent for Google Ads was granted we enable ad storage
-                if (opts.consents[opts.vars.adStorageName || 'google-ads']){
-                    console.log("Google ads usage was granted")
-                    gtag('consent', 'update', {'ad_storage': 'granted'})
                 }
             `,
             onInit: `
                 // initialization code here (will be executed only once per page-load)
                 window.dataLayer = window.dataLayer || [];
                 window.gtag = function(){dataLayer.push(arguments)}
-                gtag('consent', 'default', {'ad_storage': 'denied', 'analytics_storage': 'denied'})
+                gtag('consent', 'default', {'ad_storage': 'denied', 'analytics_storage': 'denied', 'ad_user_data': 'denied', 'ad_personalization': 'denied'})
                 gtag('set', 'ads_data_redaction', true)
             `,
-            onDecline: `
-                // initialization code here (will be executed only once per page-load)
-                window.dataLayer = window.dataLayer || [];
-                window.gtag = function(){dataLayer.push(arguments)}
-                gtag('consent', 'default', {'ad_storage': 'denied', 'analytics_storage': 'denied'})
-                gtag('set', 'ads_data_redaction', true)
-            `,
-            vars: {
-                googleAnalytics: 'google-analytics'
-            }
         },
         {
             // In GTM, you should define a custom event trigger named `klaro-google-analytics-accepted` which should trigger the Google Analytics integration.
             name: 'google-analytics',
+            cookies: [
+                /^_ga(_.*)?/ // we delete the Google Analytics cookies if the user declines its use
+            ],
+            purposes: ['marketing'],
+            onAccept: `
+                // we grant analytics storage
+                gtag('consent', 'update', {
+                    'analytics_storage': 'granted',
+                })
+            `,
+            onDecline: `
+                // we deny analytics storage
+                gtag('consent', 'update', {
+                    'analytics_storage': 'denied',
+                })
+            `,
+        },
+        {
+            name: 'google-ads',
+            cookies: [],
+            onAccept: `
+                // we grant ad storage and personalization
+                gtag('consent', 'update', {
+                    'ad_storage': 'granted',
+                    'ad_user_data': 'granted',
+                    'ad_personalization': 'granted'
+                })
+            `,
+            onDecline: `
+                // we decline ad storage and personalization
+                gtag('consent', 'update', {
+                    'ad_storage': 'denied',
+                    'ad_user_data': 'denied',
+                    'ad_personalization': 'denied'
+                })
+            `,
             purposes: ['marketing'],
         }
     ]
-};
+}
